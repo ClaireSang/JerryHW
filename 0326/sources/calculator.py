b4b3e73d6ff38adae85f0c96aa743d1bf7a3ae61
ClaireSang <claire0230025@gmail.com>
2018-03-28 10:22:25 +0800

    calculator
    calculator

---
+++ 0326/sources/calculator.py
@@ -0,0 +1,18 @@
+﻿def cal(operator,*args):
+    x1 = 0
+    x2 = 0
+
+    for i in args:
+        if x1 == 0:
+            x1 = i
+        else:
+            x2 = i
+
+    if(operator == "+"):
+        print(x1,"+",x2,"=",(x1+x2))
+    elif(operator == "-"):
+        print(x1,"-",x2,"=",(x1-x2))
+    elif(operator == "*"):
+        print(x1,"x",x2,"=",(x1*x2))
+    elif(operator == "/"):
+        print(x1,"÷",x2,"=",(x1/x2))
